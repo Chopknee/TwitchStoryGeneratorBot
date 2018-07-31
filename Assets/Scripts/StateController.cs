4da7b995ae0042161df484138f501d9f23705eb0
unknown <chopknee@gmail.com>
2018-07-31 11:59:28 -0400

    A

---
+++ Assets/Scripts/StateController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateController : MonoBehaviour {
+
+    //Three stages that need to be executed
+
+    //Stage 1: chatstorming
+    //Stage 2: elimination voting
+    //Stage 3: end and possibly repeat
+
+    enum BotState { IDLE, CHATSORM, ELIMINATION, END };
+
+    ChatBot bot;
+
+    BotState currentState = BotState.IDLE;
+
+    //All times are in seconds
+    
+    float chatstormTime = 300;
+    float eliminationRoundTime = 120;
+
+
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+        switch (currentState) {
+            case BotState.IDLE:
+                break;
+            case BotState.CHATSORM:
+                break;
+            case BotState.ELIMINATION:
+                break;
+            case BotState.END:
+                break;
+        }
+
+	}
+}
